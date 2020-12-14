2585aa6abc79d56ed6e7b957ce89b4dbc21d99e7
Yan <2031974+yangit@users.noreply.github.com>
2020-12-14 16:36:39 +0800

    init

---
+++ config.h
@@ -0,0 +1,16 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#undef ONESHOT_TIMEOUT
+#define ONESHOT_TIMEOUT 2002
+
+#undef RGBLIGHT_HUE_STEP
+#define RGBLIGHT_HUE_STEP 6
+
+#undef RGBLIGHT_BRI_STEP
+#define RGBLIGHT_BRI_STEP 6
+
+#define FIRMWARE_VERSION u8"rOb5l/LpGQL"
+#define RGB_MATRIX_STARTUP_SPD 60
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Readme
+
+Files from here are individually linked into
+
+git clone --recurse-submodules https://github.com/zsa/qmk_firmware.git
+
+rm -rf qmk_firmware/keyboards/moonlander/keymaps/yan
+cp -R yan qmk_firmware/keyboards/moonlander/keymaps
+cd qmk_firmware
+./util/docker_build.sh moonlander:yan
+cd ..
